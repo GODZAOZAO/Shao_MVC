773deb326f318c9cf1b415f0ab80dba0f5db13fc
Shao <aaronwang1023@gmail.com>
2023-05-16 18:17:03 -0700

    scaffold identity - there will be new ApplicationDbContext created : search for it and delete it. ( bad .Net :(

--- ShaoWeb/Program.cs
+++ ShaoWeb/Program.cs
@@ -2,6 +2,7 @@ using Shao.DataAccess.Repository;
 using Shao.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Shao.DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
